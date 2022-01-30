923eaf23f6551400bbed7de0fa5f80ad2fc241e7
Lukas Böhm <suluke93@gmail.com>
2022-01-30 21:54:33 +0100

    Fix wasm build
    Still need to proper implement some shortcuts that were taken. But
    overall this should hopefully unblock us for now or at least very soon.

--- src/gl_generator_websys/src/compat.rs
+++ src/gl_generator_websys/src/compat.rs
@@ -15,6 +15,35 @@ where
     Ok(())
 }
 
+#[derive(Copy, Clone)]
+pub struct FunctionAlternative {
+    name: &'static str,
+    code: &'static str,
+}
+impl FunctionAlternative {
+    pub fn get(name: &str) -> Option<Self> {
+        Self::get_all().iter().cloned().find(|alt| alt.name == name)
+    }
+    pub fn get_all() -> [Self; 2] {
+        [
+            Self {
+                name: "ShaderSource",
+                code: "#[allow(non_snake_case)]
+                pub unsafe fn ShaderSource(_shader: types::GLuint, _count: types::GLsizei, _string: *const *const types::GLchar, _length: *const types::GLint) {
+                    todo!()
+                }",
+            },
+            Self {
+                name: "GetShaderInfoLog",
+                code: "#[allow(non_snake_case)]
+                pub unsafe fn GetShaderInfoLog(_shader: types::GLuint, _bufSize: types::GLsizei, _length: *mut types::GLsizei, _infoLog: *mut types::GLchar) {
+                    todo!()
+                }",
+            },
+        ]
+    }
+}
+
 pub fn write_typdefs<W>(_registry: &Registry, dest: &mut W) -> std::io::Result<()>
 where
     W: std::io::Write,
@@ -23,13 +52,22 @@ where
     Ok(())
 }
 
-fn write_functions<W>(_registry: &Registry, dest: &mut W) -> std::io::Result<()> where W: std::io::Write {
-    writeln!(dest, "
+fn write_functions<W>(_registry: &Registry, dest: &mut W) -> std::io::Result<()>
+where
+    W: std::io::Write,
+{
+    writeln!(
+        dest,
+        "
         #[allow(non_snake_case)]
         pub unsafe fn GetShaderiv(_shader: GLuint, _name: GLenum, _buf: *mut GLint) {{
             todo!();
         }}
-    ")?;
+    "
+    )?;
+    for alt in FunctionAlternative::get_all() {
+        writeln!(dest, "{}", alt.code)?;
+    }
     Ok(())
 }
 
