5da25ea23846fbf99d53d24f02a2c84a5c878d67
Lukas Böhm <suluke93@gmail.com>
2022-01-30 19:58:55 +0100

    Reinterpret classes as numeric ids
    OpenGL ES will use GLuint/GLint instead of dedicated types. Luckily, on
    wasm32 these classes are the same size as GLuint/GLint, so this is
    possible. Unfortunately, GLUniformLocation is GLint and this change
    currently makes everything GLuint. Maybe I figure out the pattern behind
    this. Or I just fix this up manually in a separate change. Anyhow, we're
    now only 4 compile errors away it seems from being able to run in wasm
    again \o/

--- src/gl_generator_websys/src/types.rs
+++ src/gl_generator_websys/src/types.rs
@@ -8,20 +8,22 @@ enum PassBy {
 struct RustType {
     name: String,
     pass_by: PassBy,
+    optional: bool,
 }
 impl RustType {
     fn from_idl(ty: &Type, registry: &Registry) -> Self {
-        let (name, pass_by) = match &ty.kind {
-            TypeKind::Primitive(p) => (p.name().into(), PassBy::Val),
-            TypeKind::String => ("String".into(), PassBy::Ref),
+        let (name, pass_by, optional) = match &ty.kind {
+            TypeKind::Primitive(p) => (p.name().into(), PassBy::Val, ty.optional),
+            TypeKind::String => ("*const i8".into(), PassBy::Val, ty.optional),
             TypeKind::ArrayBuffer | TypeKind::ArrayBufferView | TypeKind::BufferSource => {
-                ("mut [u8]".into(), PassBy::Ref)
+                ("mut [u8]".into(), PassBy::Ref, ty.optional)
             }
-            TypeKind::CanvasElement => ("HtmlCanvasElement".into(), PassBy::Ref),
-            TypeKind::TypedArray(p) => (format!("[{}]", p.name()), PassBy::Ref),
+            TypeKind::CanvasElement => ("HtmlCanvasElement".into(), PassBy::Ref, ty.optional),
+            TypeKind::TypedArray(p) => (format!("[{}]", p.name()), PassBy::Ref, ty.optional),
             TypeKind::Sequence(ty) => (
                 format!("Vec<{}>", Self::from_idl(ty, registry).name),
                 PassBy::Ref,
+                ty.optional,
             ),
             // We mostly have Union(TypedArray | Sequence) which would map to &[PRIMITIVE] vs JsValue respectively in web_sys.
             // The latter is obviously not used in OpenGL so we flatten the Union to TypedArray only.
@@ -31,28 +33,35 @@ impl RustType {
                     .map(|ty| Self::from_idl(ty, registry).name)
                     .expect("Unsupported return type: Union without TypedArray"),
                 PassBy::Ref,
+                ty.optional,
             ),
             TypeKind::Named(name) => {
                 let resolved = registry.resolve_type(name);
                 match resolved {
                     NamedType::Mixin(_) => unimplemented!("Unsupported return type: Mixin"),
-                    NamedType::Interface(_) | NamedType::Dictionary(_) => {
-                        (name.clone(), PassBy::Ref)
+                    NamedType::Interface(_) => (name.clone(), PassBy::Val, false), // interfaces are all GLuint in GLES, None is -1/MAX
+                    NamedType::Dictionary(_) => (name.clone(), PassBy::Ref, ty.optional),
+                    NamedType::Typedef(ty) => {
+                        let alias = Self::from_idl(ty, registry);
+                        (name.clone(), alias.pass_by, alias.optional)
                     }
-                    NamedType::Typedef(ty) => (name.clone(), Self::from_idl(ty, registry).pass_by),
-                    NamedType::Enum(_) => (name.clone(), PassBy::Val),
+                    NamedType::Enum(_) => (name.clone(), PassBy::Val, ty.optional),
                     NamedType::Callback(_) => unimplemented!("Unsupported return type: Callback"),
                 }
             }
-            TypeKind::Any | TypeKind::Object => ("JsValue".into(), PassBy::Ref),
+            TypeKind::Any | TypeKind::Object => ("JsValue".into(), PassBy::Ref, ty.optional),
         };
-        Self { name, pass_by }
+        Self {
+            name,
+            pass_by,
+            optional,
+        }
     }
 }
 
 pub fn stringify_return(ty: &Type, registry: &Registry) -> String {
     let mapped = RustType::from_idl(ty, registry);
-    if ty.optional {
+    if mapped.optional {
         format!("Option<{}>", mapped.name)
     } else {
         mapped.name
@@ -66,7 +75,7 @@ pub fn stringify_arg(ty: &Type, registry: &Registry) -> String {
     } else {
         ""
     };
-    if ty.optional {
+    if mapped.optional {
         format!("Option<{}{}>", pass_spec, mapped.name)
     } else if pass_spec.is_empty() {
         mapped.name
